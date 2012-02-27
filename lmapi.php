cb98b2cca421d170b27cdfba41c4ca493225ef78
James Cornman <jizaymes@gmail.com>
2012-02-26 21:41:42 -0500

    Added centralized 'call()' function for CURL stuff

--- lmapi.php
+++ lmapi.php
@@ -16,7 +16,12 @@ class logicMonitor
 		$this->config = $config;
 		$this->isConnected = false;
 		
-		$checks = array($this->config['baseurl'],$this->config['company'],$this->config['user'],$this->config['password']);
+		$checks = array(
+			$this->config['baseurl'],
+			$this->config['company'],
+			$this->config['user'],
+			$this->config['password']
+		);
 		
 		foreach($checks as $check)
 		{
@@ -41,6 +46,7 @@ class logicMonitor
 	}
 	
 /* ----====----====----====----====----====----====----====----====----====----====----====----====----====----====----====----====----====----==== */
+
 	private function logout()
 	{
 		if($this->connected || file_exists($this->cookie)) {
@@ -48,30 +54,58 @@ class logicMonitor
 			unlink($this->cookie);
 		}
 	}
+
+/* ----====----====----====----====----====----====----====----====----====----====----====----====----====----====----====----====----====----==== */	
+
+	public function call($url,&$results,&$errMsg = null)
+	{
+
+		if(!$this->connected) { echo("Not Connected"); return false; }
+
+		$ch = curl_init();
+
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch, CURLOPT_COOKIEFILE, $this->cookie); 
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		
+		$response = json_decode(curl_exec($ch));
+		
+		if($response->status != 200) {
+			$errMsg = $response->errmsg;
+			return $response->status;
+		} else {
+			$results = $response->data;
+			return $response->status;
+		}
+	}
+	
 /* ----====----====----====----====----====----====----====----====----====----====----====----====----====----====----====----====----====----==== */	
 	
 	private function login()
 	{
-		$ch = curl_init();
-	
+
 	    $url = $this->config['baseurl'] . "signIn?c=" . $this->config['company'] . "&u=" . $this->config['user'] . "&p=" . $this->config['password'];
 
+		$ch = curl_init();
+	
 		curl_setopt($ch, CURLOPT_URL, $url);
 		curl_setopt($ch, CURLOPT_HEADER, 0);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	
+
 		$this->cookie = tempnam ($this->config['tmpdir'], "CURLCOOKIE");
 		curl_setopt($ch, CURLOPT_COOKIEJAR, $this->cookie);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 	
 		$response = json_decode(curl_exec($ch));
-		
+
 		if($response->status != 200) {
 			$this->connected = false;
 			return $response->errmsg;
 		} else {
 			$this->connected = true;
-			return true;
+			return $response->data;
 		}
 	}
 
@@ -79,68 +113,50 @@ class logicMonitor
 	
 	public function getHost($workName = "") {
 
-		if(!$this->connected) { return false; }
-	
 		$name = urlencode($workName);
 	
-		$ch = curl_init();
 		$url = $this->config['baseurl'] . "getHost?hgId=-1&displayName=$name";
+	
+		$results = null;
+		$errMsg = null;
+	
+		$response = $this->call($url,$results,$errMsg);
 
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_HEADER, 0);
-		curl_setopt($ch, CURLOPT_COOKIEFILE, $this->cookie); 
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-		
-		$response = json_decode(curl_exec($ch));
-
-		curl_close($ch);
-
-		return $response->data;	
+		return $results;	
 	}
 
 /* ----====----====----====----====----====----====----====----====----====----====----====----====----====----====----====----====----====----==== */
 	
 	public function getHostGroups($workName = "") {
 
-		if(!$this->connected) { return false; }
-	
-		$ch = curl_init();
 		$url = $this->config['baseurl'] . "getHostGroups";
 	
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_HEADER, 0);
-		curl_setopt($ch, CURLOPT_COOKIEFILE, $this->cookie); 
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-		
-		$response = json_decode(curl_exec($ch));
+		$results = null;
+		$errMsg = null;
+	
+		$response = $this->call($url,$results,$errMsg);
 		$matches = array();
-		
-		if(strlen($workName) > 0 && $response != null) {
+
+		if(strlen($workName) > 0 && $results != null) {
 
 			$name = preg_replace("/\//","\\\/",$workName);
 
-			foreach($response->data as $workLine) {
+			foreach($results as $workLine) {
 				$line=(array)$workLine;
 				
 				if(preg_match("/" . $name . "$/",$line['fullPath'])) {
 					$matches[] = $line;
 				}
 			}
-		}  else { $matches = $response->data; }
+		}  else { $matches = $results; }
 	
-		curl_close($ch);
 		
 		return $matches;
-	
 	}
 	
 /* ----====----====----====----====----====----====----====----====----====----====----====----====----====----====----====----====----====----==== */
 	
 	public function updateHostGroupProps($hostGroupNameWork,$propName,$propValueWork) {
-
-		if(!$this->connected) { return false; }
 
 		$propValue = urlencode(htmlspecialchars($propValueWork));
 
@@ -158,67 +174,48 @@ class logicMonitor
         {
         	$hostGroupId = $hostGroupNameWork;
         }
-
-	
-		$ch = curl_init();
 	
 		$url = $this->config['baseurl'] . "updateHostGroup?id=$hostGroupId&name=$hostGroupName&propName0=$propName&propValue0=$propValue";
 
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_HEADER, 0);
-		curl_setopt($ch, CURLOPT_COOKIEFILE, $this->cookie);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-		
-		$response = json_decode(curl_exec($ch));
+		$results = null;
+		$errMsg = null;
 	
-		curl_close($ch);
+		$response = $this->call($url,$results,$errMsg);
 		
-		return $response;
+		return $results;
 	}
 
 /* ----====----====----====----====----====----====----====----====----====----====----====----====----====----====----====----====----====----==== */
 	
 	public function addHostGroup($hostGroupNameWork,$descriptionWork,$parentId) {
 
-		if(!$this->connected) { return false; }
-
 		$hostGroupName = urlencode(htmlspecialchars($hostGroupNameWork));
 		$description = urlencode(htmlspecialchars($descriptionWork));
 
-                if(!is_numeric($parentId)) {
-                        $res = $this->getHostGroups($parentId);
+        if(!is_numeric($parentId)) {
+            $res = $this->getHostGroups($parentId);
 
-                        if(count($res) == 1) { //perfect
-                                $parentId = $res[0]['id'];
-                        } else {
-                                return false;
-                        }
-                }
+            if(count($res) == 1) { //perfect
+                    $parentId = $res[0]['id'];
+            } else {
+                    return false;
+            }
+        }
 
-	
-		$ch = curl_init();
 	
 		$url = $this->config['baseurl'] . "addHostGroup?name=$hostGroupName&description=$description&alertEnable=true&parentId=$parentId";
 
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_HEADER, 0);
-		curl_setopt($ch, CURLOPT_COOKIEFILE, $this->cookie);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-		
-		$response = json_decode(curl_exec($ch));
+		$results = null;
+		$errMsg = null;
 	
-		curl_close($ch);
+		$response = $this->call($url,$results,$errMsg);
 		
-		return $response;
+		return $results;
 	}
 
 /* ----====----====----====----====----====----====----====----====----====----====----====----====----====----====----====----====----====----==== */
 	
 	public function addHost($hostName,$displayName,$description,$agentId,$hostGroupId) {
-		if(!$this->connected) { return false; }
-	
 	
 		if(!is_numeric($hostGroupId)) { 
 			$res = $this->getHostGroups($hostGroupId); 
@@ -230,27 +227,21 @@ class logicMonitor
 			}
 		}
 	
-		$ch = curl_init();
 		$url = $this->config['baseurl'] . "addHost?hostName=$hostName&displayedAs=$displayName&description=$description&link=&alertEnable=true&agentId=$agentId&hostGroupIds=$hostGroupId";
+
+		$results = null;
+		$errMsg = null;
 	
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_HEADER, 0);
-		curl_setopt($ch, CURLOPT_COOKIEFILE, $this->cookie);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		$response = $this->call($url,$results,$errMsg);
+
 	
-		$response = json_decode(curl_exec($ch));
-		curl_close($ch);
-	
-		return $response;
+		return $results;
 	
 	}
 
 /* ----====----====----====----====----====----====----====----====----====----====----====----====----====----====----====----====----====----==== */
 	
 	public function deleteHost($hostName) {
-		if(!$this->connected) { return false; }
-	
 
 		if(!is_numeric($hostName)) {	
 
@@ -259,47 +250,46 @@ class logicMonitor
 			$hostId = $res->id;
 		} else { $hostId = $hostName; }
 	
-		$ch = curl_init();
 		$url = $this->config['baseurl'] . "deleteHost?hostId=$hostId&deleteFromSystem=true";
 
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_HEADER, 0);
-		curl_setopt($ch, CURLOPT_COOKIEFILE, $this->cookie);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		$results = null;
+		$errMsg = null;
 	
-		$response = json_decode(curl_exec($ch));
-		curl_close($ch);
+		$response = $this->call($url,$results,$errMsg);
 	
-		return $response;
-	
+		return $results;
 	}
 
 
 /* ----====----====----====----====----====----====----====----====----====----====----====----====----====----====----====----====----====----==== */
 	
 	public function deleteHostGroup($hostGroupName) {
-		if(!$this->connected) { return false; }
 	
 		if(!is_numeric($hostGroupName)) {	
 
 			$res = $this->getHostGroups($hostGroupName); 
-			$hostGroupId = $res[0]['id'];
+			
+			if(count($res) > 0)
+			{
+				$hostGroupId = $res[0]['id'];
+			} else
+			{
+				return "HostGroup does not exist";
+			}
 		} else { $hostGroupId = $hostGroupName; }
 
-		$ch = curl_init();
 		$url = $this->config['baseurl'] . "deleteHostGroup?hgId=$hostGroupId&deleteFromSystem=true";
 
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_HEADER, 0);
-		curl_setopt($ch, CURLOPT_COOKIEFILE, $this->cookie);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		$results = null;
+		$errMsg = null;
 	
-		$response = json_decode(curl_exec($ch));
-		curl_close($ch);
-	
-		return $response;
+		$response = $this->call($url,$results,$errMsg);
+
+		if($errMsg === true) {
+			return $errMsg;
+		} else {
+			return $results;
+		}
 	
 	}
 	
