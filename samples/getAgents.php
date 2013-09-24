30ea831102cb0846da24e71fa1145c4bbd500856
James Cornman <jizaymes@gmail.com>
2013-09-24 10:17:22 -0400

    update getAgent to filter passwords and agentConf variable

--- samples/getAgents.php
+++ samples/getAgents.php
@@ -7,11 +7,31 @@ $lm = new logicMonitor($config);
 if($lm === false) { die("Error"); }
 
 if($argc>1) { 
-	$agents = $lm->getAgents($argv[1]);
+	$rawAgents = json_decode($lm->getAgents($argv[1]));
 } else { 
-	$agents = $lm->getAgents();
+	$rawAgents = json_decode($lm->getAgents(),true);
 }
 
-print_r($agents);
+if($rawAgents['status'] != 200) { die("Error"); }
 
+$agents = array();
+$badKeys = array("agentConf","credential","escalatingChain");
+
+
+foreach($rawAgents['data'] as $agent)
+{
+	$workAgent = array();
+	foreach($agent as $key=>$val)
+	{
+		if(array_search($key,$badKeys) === false) {
+			$workAgent[$key] = $val;	
+		}
+	}
+
+	$agents[] = $workAgent;
+}
+
+echo(count($agents));
+
+print_r($agents);
 ?>
